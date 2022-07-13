4403dabb2a2fe3f20ed2b18530521e07ca04525c
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2022-07-12 21:23:04 -0400

    main

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use arduino_uno::prelude::*;
+use arduino_uno::hal::port::portb::PB5;
+use arduino_uno::hal::port::mode::Output;
+
+#[arduino_uno::entry]
+fn main() -> ! {
+    let peripherals = arduino_uno::Peripherals::take().unwrap();
+
+    let mut pins = arduino_uno::Pins::new(
+        peripherals.PORTB,
+        peripherals.PORTC,
+        peripherals.PORTD,
+    );
+
+    let mut led = pins.d13.into_output(&mut pins.ddr);
+
+    loop {
+        stutter_blink(&mut led, 25);
+    }
+}
+
+fn stutter_blink(led: &mut PB5<Output>, times: usize) {
+    (0..times).map(|i| i * 10).for_each(|i| {
+        led.toggle().void_unwrap();
+        arduino_uno::delay_ms(i as u16);
+    });
+}
